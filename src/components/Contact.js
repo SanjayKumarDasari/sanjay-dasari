2d8633688859f53550cbe035246e991151f19e8c
sanjay <sanju21101999@gmail.com>
2025-08-01 19:27:02 +0530

    corrected.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -112,10 +112,10 @@ const Contact = () => {
         <p className="intro-text">Let’s Connect, Collaborate, and Code — crafting better avenir!</p>
         <div className="contact-icons">
           <a className="cta" id="insta" href="https://www.instagram.com/sanjaykumardasari/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-          <a className="cta" id="mail" href="mailto:dsanjaykumar121@gmail.com" target="_blank" rel="noopener noreferrer"><FaEnvelope /></a>
+          <a className="cta" id="mail" href="mailto:sanjaydasari.work@gmail.com" target="_blank" rel="noopener noreferrer"><FaEnvelope /></a>
           <a className="cta" id="wapp" href="https://api.whatsapp.com/send?phone=918500434390&text=Hi%20There!" target="_blank" rel="noopener noreferrer"><FaWhatsapp /></a>
           <a className="cta" id="lin" href="https://www.linkedin.com/in/sanjay-kumar-dasari-b920b7193/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-          <a className="cta" id="tele" href="https://t.me/sanjay2110" target="_blank" rel="noopener noreferrer"><FaTelegram /></a>
+          <a className="cta" id="tele" href="https://t.me/tink543" target="_blank" rel="noopener noreferrer"><FaTelegram /></a>
         </div>
         <div className="resume-info">
           <p style={{marginTop: '2rem', color: '#b3b3b3'}}>For official considerations, my resume is available. Click the chat bubble at top right to download.</p>
